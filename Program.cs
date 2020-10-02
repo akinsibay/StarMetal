a7f046aaf3d6beb5a52ebe38585ae8b0e1909844
Otomasyon PC <bilgi@anaks.com.tr>
2020-10-02 10:45:00 +0300

    comment added

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace StarMetal
         [STAThread]
         static void Main()
         {
+            //comment
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
